9e0e6416155cfcc8a06c1536bb03939a0f2d33b2
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-08-06 18:56:28 +0530

    Styling added for chat App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import { Container, Typography, Box, TextField, Button, Divider } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
 const socket = io.connect('http://localhost:3001');
 
 const App = () => {
   const [room, setRoom] = useState('');
+  const [isShow, setIsShow] = useState(true);
+  const [username, setUsername] = useState('');
   const [message, setMessage] = useState('');
   const [receivedMessage, setReceivedMessage] = useState([]);
 
@@ -19,29 +24,69 @@ const App = () => {
   const joinRoom = () => {
     if (room !== '') {
       socket.emit('join_room', room);
+      setIsShow(false)
     }
   };
 
   const sendMessage = () => {
     const data = {
+      username,
       message,
       room,
       date: new Date(Date.now()).toLocaleTimeString(),
     };
     socket.emit('send_message', data);
     setReceivedMessage((last) => [...last, data]);
+    setMessage('');
   };
 
   return (
-    <div>
-      <input value={room} placeholder='Enter room' onChange={(e) => setRoom(e.target.value)}></input>
-      <button onClick={joinRoom}>Join Room</button>
-      <input value={message} placeholder='Enter message' onChange={(e) => setMessage(e.target.value)}></input>
-      <button onClick={sendMessage}>Send message</button>
-      <h1>Received Message </h1>
-
-      <Data receivedMessage={receivedMessage} />
-    </div>
+    <Container maxWidth="sm">
+      <Box mt={2} textAlign="center">
+        <Typography variant="h4">Welcome to Live Chat App</Typography>
+      </Box>
+      {isShow && <><Box mt={2} display="flex" justifyContent="center">
+        <TextField
+          value={username} size='small'
+          label="Username"
+          variant="outlined"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Box>
+      <Box mt={2} display="flex" justifyContent="center">
+        <TextField
+          value={room} size='small'
+          label="Room"
+          variant="outlined"
+          onChange={(e) => setRoom(e.target.value)}
+        />
+      </Box>
+      <Box mt={2} display="flex" justifyContent="center">
+        <Button variant="contained" color="primary" onClick={joinRoom}>
+          Join Room
+        </Button>
+      </Box></> }
+      <Box mt={4}>
+        <Divider variant="middle" />
+        <Box mt={2}>
+          <div className="chat-messages">
+            <Data receivedMessage={receivedMessage} />
+          </div>
+        </Box>
+      </Box>
+      <Box mt={2} display="flex" alignItems="center">
+        <TextField
+          value={message}
+          label="Type a message..."
+          variant="outlined"
+          onChange={(e) => setMessage(e.target.value)}
+          fullWidth size='small'
+        />
+        <Button style={{marginLeft:20}} variant="contained" color="primary" onClick={sendMessage}>
+          <SendIcon />
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
@@ -49,7 +94,13 @@ const Data = ({ receivedMessage }) => {
   return (
     <div>
       {receivedMessage.map((msg, index) => {
-        return <p key={index}>{msg.message} <span>{msg.date}</span></p>;
+        return (
+          <p key={index} className={`message ${msg.username === 'You' ? 'sent' : 'received'}`}>
+            <span className="username">{msg.username}</span>
+            <span className="message-text">{msg.message}</span>
+            <span className="date">{msg.date}</span>
+          </p>
+        );
       })}
     </div>
   );
